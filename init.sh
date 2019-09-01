088664871574f424ce1a107c6c14e7577c466db0
nikitos199 <nik.vin2012@yandex.ru>
2019-09-01 13:36:37 +0400

    7 commit

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabeles/default
-sudo service nginx stop
-sudo nginx -c /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/dafault
+sudo etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
 cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
