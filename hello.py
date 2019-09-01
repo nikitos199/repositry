49f3b7908281b159a354f43e6abee34ef4c265d6
nikitos199 <nik.vin2012@yandex.ru>
2019-09-01 12:14:57 +0400

    second type

---
+++ hello.py
@@ -0,0 +1,7 @@
+def application(env,start_response):
+	print(env,['QUERY_STRING'])
+	data = env['QUERY_STRING'].split(&)
+	data ='\n'.join(data)
+	data = bytes(data, encoding='utf-8')
+	start_response('200 OK',[('Content-Type','text/plain')])
+	return [data]
--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
+cd /home/box/web && sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
